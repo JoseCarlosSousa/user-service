64b085dc00b93b65e8aabb53672fa6dbace8b010
Carlos Sousa <kkosmico@gmail.com>
2026-08-22 21:24:15 +0100

    fine

--- src/main/java/pt/kkosmico/dto/ErrorResponseDTO.java
+++ src/main/java/pt/kkosmico/dto/ErrorResponseDTO.java
@@ -15,12 +15,4 @@ public class ErrorResponseDTO {
     private String error;
     private String message;
     private String path;
-
-    public ErrorResponseDTO(LocalDateTime timestamp, int status, String error, String message, String path) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
 }
