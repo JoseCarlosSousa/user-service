e58567da94fc1c357b9b1beca8f9955486f7041a
Carlos Sousa <kkosmico@gmail.com>
2026-08-24 02:12:59 +0100

    tabelas

--- src/main/resources/db/migration/V1__Create_Table_Users.sql
+++ src/main/resources/db/migration/V1__Create_Table_Users.sql
@@ -5,6 +5,8 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50) DEFAULT 'USER',
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
     active BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (id)
 );
--- src/main/resources/db/migration/V2__Create_Table_Customers.sql
+++ src/main/resources/db/migration/V2__Create_Table_Customers.sql
@@ -2,16 +2,17 @@ DROP TABLE IF EXISTS customers;
 
 CREATE TABLE customers (
     id BINARY(16) NOT NULL,
-    user_id BINARY(16) NULL,
+    user_id BINARY(16) NOT NULL UNIQUE,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     phone_prefix VARCHAR(10) NULL,
-    phone_number VARCHAR(20) NOT NULL,
+    phone_number VARCHAR(20) NULL,
     gender VARCHAR(20) NULL,
     address VARCHAR(255) NULL,
     city VARCHAR(100) NULL,
     state VARCHAR(100) NULL,
     zip_code VARCHAR(20) NULL,
     country VARCHAR(100) NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CONSTRAINT fk_customer_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
